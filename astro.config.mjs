652c0bc809957985be996e675907d868faf1f42d
Javier Ocampo <ferroxido@gmail.com>
2025-09-04 00:47:54 +0100

    chore: Deploy with github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://javieroc.github.io',
+	base: 'the-bottle-of-klein',
   vite: {
     plugins: [tailwindcss()]
   }
